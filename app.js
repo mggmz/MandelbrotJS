51d91bc4123e1ac54f7acc75e8c26deae4a577b6
mggmz <tonykill3@gmail.com>
2025-03-04 12:32:22 -0600

    Added draw functionality on app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 function setup(){
     createCanvas(700, 500)
+    noLoop()
+    draw()
 }
 
 function draw(){
-    background(0)
+    loadPixels()
+    for(let x = 0; x < width; x++){
+        for(let y = 0; y < width; y++){
+            let pos_pix = (x + y * width) * 4
+            pixels[pos_pix] = 0  // R  
+            pixels[pos_pix + 1] = 0 // G
+            pixels[pos_pix + 2] = 0  // B
+            pixels[pos_pix + 3] = 255 // Alpha
+
+    }
+}
+    updatePixels()
 }
